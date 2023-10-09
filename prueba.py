d07269e08c893da47eec05147e2fd77dab4d91be
reima <78701493+reinier03@users.noreply.github.com>
2023-10-09 20:57:13 +0200

    Se han modificado los archivos y se ha actualizado el codigo

--- prueba.py
+++ prueba.py
@@ -1,8 +1,19 @@
 import telebot
 import os
-
+from flask import Flask, request
+from waitress import serve
+import time
 
 bot=telebot.TeleBot("5818205719:AAHk-liE0DD4S5ltg-kFN88Ckn4CTBUmMNc")
+web_server= Flask(__name__)
+
+@web_server.route("/", methods=["POST"])
+def webhook():
+    if request.headers.get("content-type") == "aplication/json":
+        update=telebot.types.Update.de_json(request.stream.read().decode("utf-8"))
+        bot.process_new_updates([update])
+        return "OK", 200
+
 
 @bot.message_handler(commands=["start"])
 def cmd_star(message):
@@ -13,4 +24,7 @@ def cmd_recibir_mensajes(message):
     bot.send_message(message.chat.id, "El bot funciona")
     
 
-bot.infinity_polling()
+bot.remove_webhook
+time.sleep(1)
+bot.set_webhook(url="https://api.render.com/deploy/srv-ckf54q6afg7c73fo3bb0?key=KJ29aU6GkhI")
+serve(web_server, host="0.0.0.0", port=5000)
