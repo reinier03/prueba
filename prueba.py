55da80223ab469d1a116c38a0d5b0b6c35146367
reima <78701493+reinier03@users.noreply.github.com>
2023-10-09 22:19:33 +0200

    isjdfijsdif

--- prueba.py
+++ prueba.py
@@ -7,6 +7,10 @@ import time
 bot=telebot.TeleBot("5818205719:AAHk-liE0DD4S5ltg-kFN88Ckn4CTBUmMNc")
 web_server= Flask(__name__)
 
+bot.set_my_commands([
+    telebot.types.BotCommand("/start", "Da la bienvenida, o algo así")
+])
+
 @web_server.route("/", methods=["POST"])
 def webhook():
     update = telebot.types.Update.de_json(request.get_json(force=True))
@@ -24,8 +28,10 @@ def cmd_recibir_mensajes(message):
 
 bot.send_message(1413725506, "Estoy online :D")
     
+def iniciar_webhook():
+    bot.remove_webhook
+    time.sleep(1)
+    bot.set_webhook(url="https://api.render.com/deploy/srv-ckf54q6afg7c73fo3bb0?key=KJ29aU6GkhI")
+    serve(web_server, host="0.0.0.0", port=80)
 
-bot.remove_webhook
-time.sleep(1)
-bot.set_webhook(url="https://api.render.com/deploy/srv-ckf54q6afg7c73fo3bb0?key=KJ29aU6GkhI")
-serve(web_server, host="0.0.0.0", port=80)
+iniciar_webhook()
