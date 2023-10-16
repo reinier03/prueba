22ad15c51ce5d0d5f1f678b430fa8fa4f5fe53ed
reima <78701493+reinier03@users.noreply.github.com>
2023-10-16 04:26:30 +0200

    sidjfisdjf

--- prueba.py
+++ prueba.py
@@ -4,6 +4,7 @@ import telebot
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 import os
 import time
+import threading
 
 #-----------------------------Variables necesarias---------------------------
 bot=telebot.TeleBot("5818205719:AAHk-liE0DD4S5ltg-kFN88Ckn4CTBUmMNc")
@@ -30,8 +31,6 @@ def iniciar_webhook():
 
 bot.send_message(Reima, "Estoy online :D")
 
-iniciar_webhook()
-
 
 if os.name=="nt":
     last_botonera=open(f"{directorio_actual}\\Last_Botonera.jpg", "rb")
@@ -40,69 +39,69 @@ else:
 
 
 
+def iniciar_bucle():
+    while True:
+        last_botonera.seek(0)
+        #comprobar si el so es windows
+        if os.name=='nt':
+            if os.path.isfile(f"{directorio_actual}\\archivo_canales.txt"):
+                botonera=InlineKeyboardMarkup(row_width=1)
+                with open(f"{directorio_actual}\\archivo_canales.txt", "r+") as archivo:
+                    archivo.seek(0)
+                    lineas=archivo.readlines()
+                    for linea in lineas:
+                        botonera.add(InlineKeyboardButton(bot.get_chat(linea.strip()).title, url=f"https://t.me/{bot.get_chat(linea.strip()).username}"))
 
-while True:
-    last_botonera.seek(0)
-    #comprobar si el so es windows
-    if os.name=='nt':
-        if os.path.isfile(f"{directorio_actual}\\archivo_canales.txt"):
-            botonera=InlineKeyboardMarkup(row_width=1)
-            with open(f"{directorio_actual}\\archivo_canales.txt", "r+") as archivo:
-                archivo.seek(0)
-                lineas=archivo.readlines()
-                for linea in lineas:
-                    botonera.add(InlineKeyboardButton(bot.get_chat(linea.strip()).title, url=f"https://t.me/{bot.get_chat(linea.strip()).username}"))
+                        if not bot.get_chat_member(chat_id=linea.strip(), user_id=bot.user.id).status=="administrator":
+                            bot.send_message(1413725506, f"<u>ATENCION!</u>:\nNO soy admin en @{bot.get_chat(linea.strip).username}, ID: {linea.strip()}")
+                    botonera.add(InlineKeyboardButton("(☞ﾟヮﾟ)☞ UNIRSE AQUÍ ☜(ﾟヮﾟ☜)", "https://t.me/mistakedelalaif"))
+                    archivo.seek(0)
+                    for e,linea in enumerate(lineas, start=0):
+                        last_botonera.seek(0)
+                        try:
+                            msg=bot.send_photo(linea.strip(), last_botonera, caption="¡Si!, ¡Es eso mismo que estás pensando!\n Literalmente, <b>La Última Botonera</b> baby (☞ﾟヮﾟ)☞ ☜(ﾟヮﾟ☜)\n\n¡Oye Juan! ¿Quién hizo ese logo? ¡Quiero tatuarme eso en el c*lo!", parse_mode="html" , reply_markup=botonera)
+                            bot.delete_message(linea.strip(), msg.id)
+                        except:
+                            continue
+            #si el archivo de texto no existe    
+            else:
+                if os.name=="nt":
+                    bot.send_message(Reima, "No había archivo de texto con los canales\nProcederé a crearlo")
+                    with open(f"{directorio_actual}\\archivo_canales.txt", "w") as archivo:
+                        archivo.write("-1001161864648\n")
+                else:
+                    bot.send_message(Reima, "No había archivo de texto con los canales\nProcederé a crearlo")
+                    with open(f"{directorio_actual}//archivo_canales.txt", "w") as archivo:
+                        archivo.write("-1001161864648\n")
 
-                    if not bot.get_chat_member(chat_id=linea.strip(), user_id=bot.user.id).status=="administrator":
-                        bot.send_message(1413725506, f"<u>ATENCION!</u>:\nNO soy admin en @{bot.get_chat(linea.strip).username}, ID: {linea.strip()}")
-                botonera.add(InlineKeyboardButton("(☞ﾟヮﾟ)☞ UNIRSE AQUÍ ☜(ﾟヮﾟ☜)", "https://t.me/mistakedelalaif"))
-                archivo.seek(0)
-                for e,linea in enumerate(lineas, start=0):
-                    last_botonera.seek(0)
-                    try:
-                        msg=bot.send_photo(linea.strip(), last_botonera, caption="¡Si!, ¡Es eso mismo que estás pensando!\n Literalmente, <b>La Última Botonera</b> baby (☞ﾟヮﾟ)☞ ☜(ﾟヮﾟ☜)\n\n¡Oye Juan! ¿Quién hizo ese logo? ¡Quiero tatuarme eso en el c*lo!", parse_mode="html" , reply_markup=botonera)
-                        bot.delete_message(linea.strip(), msg.id)
-                    except:
-                        continue
-        #si el archivo de texto no existe    
+        #Si no se ejecuta en Windows sino que en Linux (o Mac)
         else:
-            if os.name=="nt":
-                bot.send_message(Reima, "No había archivo de texto con los canales\nProcederé a crearlo")
-                with open(f"{directorio_actual}\\archivo_canales.txt", "w") as archivo:
-                    archivo.write("-1001161864648\n")
+            if os.path.isfile(f"{directorio_actual}//archivo_canales.txt"):
+                botonera=InlineKeyboardMarkup(row_width=1)
+                with open(f"{directorio_actual}//archivo_canales.txt", "r+") as archivo:
+                    archivo.seek(0)
+                    lineas=archivo.readlines()
+                    for linea in lineas:
+                        botonera.add(InlineKeyboardButton(bot.get_chat(linea.strip()).title, url=f"https://t.me/{bot.get_chat(linea.strip()).username}"))
+
+                        if not bot.get_chat_member(chat_id=linea.strip(), user_id=bot.user.id).status=="administrator":
+                            bot.send_message(1413725506, f"<u>ATENCION!</u>:\nNO soy admin en @{bot.get_chat(linea.strip).username}, ID: {linea.strip()}")
+        
+                    botonera.add(InlineKeyboardButton("(☞ﾟヮﾟ)☞ UNIRSE AQUÍ ☜(ﾟヮﾟ☜)", url="https://t.me/mistakedelalaif"))
+                    archivo.seek(0)
+                    for e,linea in enumerate(lineas, start=0):
+                        last_botonera.seek(0)
+                        try:
+                            bot.send_photo(linea.strip(), last_botonera, caption="¡Si!, ¡Es eso mismo que estás pensando!\n Literalmente, <b>La Última Botonera</b> baby (☞ﾟヮﾟ)☞ ☜(ﾟヮﾟ☜)\n\n¡Oye Juan! ¿Quién hizo ese logo? ¡Quiero tatuarme eso en el c*lo!", parse_mode="html" , reply_markup=botonera)
+                        except:
+                            continue
+            #si el archivo de texto no existe    
             else:
                 bot.send_message(Reima, "No había archivo de texto con los canales\nProcederé a crearlo")
                 with open(f"{directorio_actual}//archivo_canales.txt", "w") as archivo:
                     archivo.write("-1001161864648\n")
+        bot.send_message(Reima, "ya he enviado la botonera")
+        time.sleep(21600)
 
-    #Si no se ejecuta en Windows sino que en Linux (o Mac)
-    else:
-        if os.path.isfile(f"{directorio_actual}//archivo_canales.txt"):
-            botonera=InlineKeyboardMarkup(row_width=1)
-            with open(f"{directorio_actual}//archivo_canales.txt", "r+") as archivo:
-                archivo.seek(0)
-                lineas=archivo.readlines()
-                for linea in lineas:
-                    botonera.add(InlineKeyboardButton(bot.get_chat(linea.strip()).title, url=f"https://t.me/{bot.get_chat(linea.strip()).username}"))
-
-                    if not bot.get_chat_member(chat_id=linea.strip(), user_id=bot.user.id).status=="administrator":
-                        bot.send_message(1413725506, f"<u>ATENCION!</u>:\nNO soy admin en @{bot.get_chat(linea.strip).username}, ID: {linea.strip()}")
-    
-                botonera.add(InlineKeyboardButton("(☞ﾟヮﾟ)☞ UNIRSE AQUÍ ☜(ﾟヮﾟ☜)", url="https://t.me/mistakedelalaif"))
-                archivo.seek(0)
-                for e,linea in enumerate(lineas, start=0):
-                    last_botonera.seek(0)
-                    try:
-                        bot.send_photo(linea.strip(), last_botonera, caption="¡Si!, ¡Es eso mismo que estás pensando!\n Literalmente, <b>La Última Botonera</b> baby (☞ﾟヮﾟ)☞ ☜(ﾟヮﾟ☜)\n\n¡Oye Juan! ¿Quién hizo ese logo? ¡Quiero tatuarme eso en el c*lo!", parse_mode="html" , reply_markup=botonera)
-                    except:
-                        continue
-        #si el archivo de texto no existe    
-        else:
-            bot.send_message(Reima, "No había archivo de texto con los canales\nProcederé a crearlo")
-            with open(f"{directorio_actual}//archivo_canales.txt", "w") as archivo:
-                archivo.write("-1001161864648\n")
-    bot.send_message(Reima, "ya he enviado la botonera")
-    time.sleep(21600)
-
-
-
+hilo_bucle=threading.Thread(name="hilo_bucle", target=iniciar_bucle)
+iniciar_webhook()
