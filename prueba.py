68de1825cfc5dae6a6192af77a85e0cd1e9c8ea0
reima <78701493+reinier03@users.noreply.github.com>
2023-10-16 04:08:38 +0200

    iofgjdfigj

--- prueba.py
+++ prueba.py
@@ -1,3 +1,5 @@
+from flask import Flask, request
+from waitress import serve
 import telebot
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 import os
@@ -9,6 +11,21 @@ Reima=1413725506
 directorio_actual=os.path.dirname(os.path.abspath(__file__))
 #----------------------------------------------------------------------------
 
+
+web_server= Flask(__name__)
+
+@web_server.route("/", methods=["POST"])
+def webhook():
+    if request.headers.get("content-type") == "aplication/json":
+        update = telebot.types.Update.de_json(request.stream.read().decode("utf-8"))
+        bot.process_new_updates([update])
+        return "OK", 200
+    
+def iniciar_webhook():
+    bot.remove_webhook()
+    time.sleep(1)
+    bot.set_webhook(url="https://api.render.com/deploy/srv-ckf54q6afg7c73fo3bb0?key=KJ29aU6GkhI")
+    serve(web_server, host="0.0.0.0", port=int(os.environ.get('PORT'),80))
 
 
 
