5d552f4f68320509a0b8f51c4e2e1a63f7da38ee
reima <78701493+reinier03@users.noreply.github.com>
2023-10-16 06:17:32 +0200

    isdjfisjdfi

--- prueba.py
+++ prueba.py
@@ -101,7 +101,7 @@ def iniciar_webhook():
     bot.remove_webhook()
     time.sleep(1)
     bot.set_webhook(url="https://api.render.com/deploy/srv-ckf54q6afg7c73fo3bb0?key=KJ29aU6GkhI")
-    serve(web_server, host="0.0.0.0", port=int(os.environ.get('PORT'),80))
+    serve(web_server, host="0.0.0.0", port=int(os.environ.get('PORT')))
 
 
 print(os.environ.get("PORT"))
