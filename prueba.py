0f054671ceb20bae5b4507f099844d2afb7a595e
reima <78701493+reinier03@users.noreply.github.com>
2023-10-27 15:52:58 -0400

    isjdifjsidfji

--- prueba.py
+++ prueba.py
@@ -1,8 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import os
-from flask import Flask, request
-from waitress import serve
+
 
 
 API_ID = os.environ.get("API_ID")       
@@ -16,20 +15,5 @@ bot = Client("@LastHopePrueba_bot", api_id=API_ID, api_hash=API_HASH, bot_token=
 def cmd_start(bot, message):
     bot.send_message(message.chat.id, "Hola :D")
 
-web_server= Flask(__name__)
-
-@web_server.route("/", methods=["POST"])
-def webhook():
-    update = request.get_json()
-    bot.process_update(update)
-    return "OK"
-
-def iniciar_webhook():
-    bot.set_webhook(url=os.environ.get("URL"))
-    serve(web_server, host="0.0.0.0", port=int(os.environ.get('PORT')))
-
-
-iniciar_webhook()
-
-
+bot.run()
 
